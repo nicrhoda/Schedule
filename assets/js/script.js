453acf8570f4a931aca7cf00fa80fc89777ee723
nic <nrhoda14@gmail.com>
2022-10-30 13:52:42 -0500

    added a save button click event that saves user input to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,19 @@
-//function for updating the current time
-//function for the saveBtn click event
-//function for updating to the current hour
-//save items to users local storage
-date();
-function date() {
-    var currentDate = moment().format('MMM d, YYYY');
-    $('#currentDay').text(currentDate);
-}
 
-console.log(date());
+//save button variable
+var saveButton = $(".saveBtn");
+//sets the current date at the top of the page 
+date();
+
+function date() {
+    var currentDate = moment().format('MMM dd, YYYY');
+    $('.currentDay').text(currentDate);
+}
+//event listener for the save button to save to local storage
+saveButton.on("click", function() {
+    var time = $(this).siblings(".time").text();
+    var input = $(this).siblings(".description").val();
+
+    localStorage.setItem(time, input);
+    console.log(time, input);
+})
+
