5b9ceaadf0f194774a4b70d8d8bad76b140a560a
nic <nrhoda14@gmail.com>
2022-10-30 20:50:07 -0500

    added timeBlockColor function to script.js and updated the README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Schedule
+
+##Description
+
+Web application takes user input through time blocks for every hour of the work day.  It then saves them to local storage when the user clicks the save box, and displays the input from local storage when the page is refreshed.  The user will also receive an alert notifying them that they have saved their changes.
+
+##URL
+
+https://nicrhoda.github.io/Schedule/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
-
-//save button variable
-var saveButton = $(".saveBtn");
-//sets the current date at the top of the page 
 date();
-colorCode();
+timeBlockColor();
 persist();
+
+
+//sets the current date at the top of the page 
 function date() {
     var currentDate = moment().format('MMM DD, YYYY');
     $('.currentDay').text(currentDate);
 }
+
+//save button variable
+var saveButton = $(".saveBtn");
+
 //event listener for the save button to save to local storage
 saveButton.on("click", function() {
     var time = $(this).siblings(".time").text();
@@ -16,6 +19,9 @@ saveButton.on("click", function() {
 
     localStorage.setItem(time, input);
     console.log(time, input);
+
+    //gives an alert that the input has been saved to local storage
+    alert("saved to local storage");
 })
 
 //load save data so the text persists on the screen after refresh
@@ -40,3 +46,83 @@ function persist() {
     $("#5pm .description").append(fiveDescription);
 }
 
+//color coding the time blocks
+
+//maybe find better solution if time allows!
+
+function timeBlockColor () {
+    var currentTime = moment().hours();
+    //9 am
+    if (currentTime === 9) {
+        $("#9am").addClass("present");
+    } else if (currentTime > 9) {
+        $("#9am").addClass("past");
+    } else {
+        $("#9am").addClass("future");
+    }
+    //10 am
+    if (currentTime === 10) {
+        $("#10am").addClass("present");
+    } else if (currentTime > 10) {
+        $("#10am").addClass("past");
+    } else {
+        $("#10am").addClass("future");
+    }
+    //11 am
+    if (currentTime ===11) {
+        $("#11am").addClass("present");
+    } else if (currentTime > 11) {
+        $("#11am").addClass("past");
+    } else {
+        $("#11am").addClass("future");
+    }
+    //12 pm
+    if (currentTime === 12) {
+        $("#12pm").addClass("present");
+    } else if (currentTime > 12) {
+        $("#12pm").addClass("past");
+    } else {
+        $("#12pm").addClass("future");
+    }
+    //1 pm
+    if (currentTime === 13) {
+        $("#1pm").addClass("present");
+    } else if (currentTime > 13) {
+        $("#1pm").addClass("past");
+    } else {
+        $("#1pm").addClass("future");
+    }
+    //2 pm
+    if (currentTime === 14) {
+        $("#2pm").addClass("present");
+    } else if (currentTime > 14) {
+        $("#2pm").addClass("past");
+    } else {
+        $("#2pm").addClass("future");
+    }
+    //3 pm
+    if (currentTime === 15) {
+        $("#3pm").addClass("present");
+    } else if (currentTime > 15) {
+        $("#3pm").addClass("past");
+    } else {
+        $("#3pm").addClass("future");
+    }
+    //4 pm
+    if (currentTime === 16) {
+        $("#4pm").addClass("present");
+    } else if (currentTime > 16) {
+        $("#4pm").addClass("past");
+    } else {
+        $("#4pm").addClass("future");
+    }
+    //5 pm
+    if (currentTime === 17) {
+        $("#5pm").addClass("present");
+    } else if (currentTime > 17) {
+        $("#5pm").addClass("past");
+    } else {
+        $("#5pm").addClass("future");
+    }
+
+}
